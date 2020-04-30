daf4f0e7cf13d25608ee57a6f6d3d4b576017d47
GhadaKareem <pbeghada@gmail.com>
2020-04-30 16:47:36 +0200

    AI Arabic Assistant

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from keras.models import Model
 from keras.layers import Input, Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 import os
-import re
+import playsound
 from glob import glob
 import numpy as np
 import pandas as pd
@@ -16,7 +16,9 @@ from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 import sounddevice as sd
 import soundfile as sf
-
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from gtts import gTTS
 
 class DatasetGenerator():
     def __init__(self, label_set,
@@ -31,53 +33,29 @@ class DatasetGenerator():
 
     # Reverse translation of numerical classes back to characters
     def labels_to_text(self, labels):
-        print("labels :"+str(labels))
+        #print("labels :"+str(labels))
         return self.label_set[labels]
 
     def load_data(self, DIR):
         # Get all paths inside DIR that ends with wav
         data = []
         for i in range(len(DIR)):
-            #print(str(len(DIR)))
-            #print("i :" + str(i))
             for filename in glob(os.path.join(DIR[i], '*.wav')):
                 wav_files = filename
-                #  print("wave file : " + wav_files)
                 show = wav_files.split('\\')
+
                 label = show[2]
                 name = show[3]
-                # print("label  " + str(label))
-                # print("name   " + str(name))
                 if label in self.label_set:
                     label_id = self.text_to_labels(label)
                     fle = os.path.join(DIR[i], name)
                     sample = (label, label_id, name, fle)
                     data.append(sample)
-        print("data here : " + str(data))
-
+        #print(str(data))
         # Data Frames with samples' labels and paths
         df = pd.DataFrame(data, columns=['label', 'label_id', 'user_id', 'wav_file'])
 
         self.df = df
-
-        """
-        for filename in glob(os.path.join(DIR, '*.wav')):
-            wav_files = filename
-            show = wav_files.split('\\')
-            name = show[3]
-            with open("file.txt", "a", encoding="utf-8") as f:
-                    f.writelines(name)
-                    f.write("\n")
-
-            show = name.split('.')
-            label = re.sub(r"[@#%$^()1234567890-Copy]", "", show[0])
-            if label in self.label_set:
-                label_id = self.text_to_labels(label)
-                fle = os.path.join(DIR, name)
-                sample = (label, label_id, name, fle)
-                data.append(sample)
-        f.close()
-        """
 
         return self.df
 
@@ -141,25 +119,26 @@ class DatasetGenerator():
                 ids = list(range(df.shape[0]))
             elif mode == 'test':
                 df = self.df_test
+                #print("df test:  p " + str(df))
                 ids = list(range(df.shape[0]))
             elif mode == 'prediction':
                 X_batch = []
                 X_batch.append(self.process_wav_file(r"C:\Users\Ghada Gamal\Desktop\Record.wav"))
                 X_batch = np.array(X_batch)
+
                 yield X_batch
             else:
                 raise ValueError('The mode should be either train, val or test.')
 
             # Create batches (for training data the batches are randomly permuted)
             if mode != 'prediction':
-                #print("len(ids)    " + str(len(ids)))
+
                 for start in range(0, len(ids), batch_size):
                     X_batch = []
                     if mode != 'test':
                         y_batch = []
                     end = min(start + batch_size, len(ids))
                     i_batch = ids[start:end]
-                   # print("i_batch    "+str(i_batch))
                     for i in i_batch:
                         X_batch.append(self.process_wav_file(df.wav_file.values[i]))
                         if mode != 'test':
@@ -172,16 +151,53 @@ class DatasetGenerator():
                     else:
                         yield X_batch
 
-DIR = ["D:\Speech_GP\داتابيز","D:\Speech_GP\سؤال عن الحال","D:\Speech_GP\قراءه التقرير الطبي","D:\Speech_GP\سؤال الدكتور","D:\Speech_GP\رفض","D:\Speech_GP\رعايه نفسيه","D:\Speech_GP\نعم","D:\Speech_GP\اعتذار","D:\Speech_GP\اوامر","D:\Speech_GP\التعريف بالنفس","D:\Speech_GP\وقت البدايه"]     # train and test data
-
+DIR = ["D:\Speech_GP\سؤال الدكتور","D:\Speech_GP\رعايه نفسيه","D:\Speech_GP\التقرير الطبي","D:\Speech_GP\قياس معدل ضربات القلب و الضغط","D:\Speech_GP\قياس درجة الحرارة" ]
+#DIR = ["سؤال الدكتور","رعايه نفسيه","قراءه التقرير الطبي","قياس الضغط و معدل ضربات القلب","قياس درجه الحراره"]
 INPUT_SHAPE = (177,98,1)
 BATCH = 32
 EPOCHS = 15
 
 with open("demofile.txt", "r", encoding="utf-8") as f:
     LABELS = [line.rstrip() for line in f]
+
 print("list of labels:" + str(LABELS))
 NUM_CLASSES = len(LABELS)
+chatbot = ChatBot("RobotNurse")
+trainer = ListTrainer(chatbot)
+trainer.train([
+    "التقرير الطبي",
+    "سأقرأ التقرير الطبي",
+])
+trainer.train([
+    "التقرير الطبي",
+    "سأقرأ التقرير الطبي",
+])
+trainer.train([
+    "رعايه نفسيه",
+    "إهدأ لا داعي للقلق",
+])
+trainer.train([
+    "سؤال الدكتور",
+    "سأتصل بالدكتور حالا",
+])
+
+trainer.train([
+    "سؤال الدكتور",
+    "سأتصل بالطبيب حالا",
+])
+trainer.train([
+    "رعايه نفسيه",
+    "إهدأ ستكون بخير",
+])
+trainer.train([
+    "قياس معدل ضربات القلب و الضغط",
+    "حاضر",
+])
+trainer.train([
+    "قياس درجة الحرارة",
+    "سأقيس درجة الحرارة",
+])
+
 
 dsGen = DatasetGenerator(label_set=LABELS)
 # Load DataFrame with paths/labels for training and validation data
@@ -229,14 +245,13 @@ def deep_cnn(features_shape, num_classes, act='relu'):
 def train ():
     model = deep_cnn(INPUT_SHAPE, NUM_CLASSES)
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['acc'])
-    #callbacks = EarlyStopping(monitor='val_acc', patience=4, verbose=1, mode='max')
-    # callbacks=[callbacks, mc],
 
     mc = ModelCheckpoint('best_model.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-
+    callbacks = EarlyStopping(monitor='val_acc', patience=4, verbose=1, mode='max')
     history = model.fit_generator(generator=dsGen.generator(BATCH, mode='train'),
                                   steps_per_epoch=int(np.ceil(len(dsGen.df_train) / BATCH)),
                                   epochs=EPOCHS,
+                                  callbacks=[callbacks, mc],
                                   verbose=1,
                                   validation_data=dsGen.generator(BATCH, mode='val'),
                                   validation_steps=int(np.ceil(len(dsGen.df_val) / BATCH)))
@@ -248,8 +263,9 @@ def train ():
 
     pre = []
     for cell in y_pred.flatten():
-        print("cell :"+ str(cell))
+        #print("cell :"+ str(cell))
         pre = dsGen.labels_to_text(cell)
+        #print("you mean: " + str(pre))
 
     y_true = dsGen.df_test['label_id'].values
 
@@ -258,34 +274,65 @@ def train ():
     print(acc_score)
 
 def predict():
-    samplerate = 16000
-    duration = 3  # seconds
-    filename = r'C:\Users\Ghada Gamal\Desktop\Record.wav'
-    print("start: ")
-    mydata = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, blocking=True)
-    print("end ")
-    sd.wait()
-    sf.write(filename, mydata, samplerate)
     model = deep_cnn(INPUT_SHAPE, NUM_CLASSES)
     model = load_model('best_model.hdf5')
-
-    y_pred_proba = model.predict_generator(dsGen.generator(BATCH, mode='prediction'),
-                                           int(np.ceil(len(dsGen.df_test) / BATCH)), verbose=1)
-
-    y_pred = np.argmax(y_pred_proba, axis=1)
-
-    pre = []
-    for cell in y_pred.flatten():
-        pre = dsGen.labels_to_text(cell)
-
-    print("you said: " + str(pre))
-def main():
     while True :
-        User_input = input("Enter train or Prediction: ").lower()
-        if User_input == 'train':
-            train()
+        User_input = input("Are you ready y or n: ").lower()
+        if User_input == 'n':
+            break
+        elif User_input == 'y':
+            samplerate = 16000
+            duration = 3  # seconds
+            filename = r'C:\Users\Ghada Gamal\Desktop\Record.wav'
+            print("Speak! ")
+            mydata = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, blocking=True)
+            print("Stop!! ")
+            sd.wait()
+            sf.write(filename, mydata, samplerate)
+            y_pred_proba = model.predict_generator(dsGen.generator(BATCH, mode='prediction'),int(np.ceil((len(dsGen.df_test)+len(dsGen.df_train)) / BATCH)),verbose=1)
+            y_pred = np.argmax(y_pred_proba, axis=1)
+            pre = []
+            lst=list(y_pred.flatten())
+           # print("count 0 " + str(lst.count(0)))
+           # print("count 1 " + str(lst.count(1)))
+           # print("count 2 " + str(lst.count(2)))
+           # print("count 3 " + str(lst.count(3)))
+           # print("count 4 " + str(lst.count(4)))
+            max_index = 0
+            for i in range(len(LABELS)):
+                if lst.count(max_index) < lst.count(i):
+                    max_index = i
+            pre = dsGen.labels_to_text(max_index)
+
+            #print("you mean: " + str(pre))
+
+            Seq2Seq(str(pre))
         else:
-            predict()
+            print('Wrong Input!')
+            continue
+
+def Seq2Seq(prediction_word):
+    try:
+        bot_input = chatbot.get_response(prediction_word)
+        print(bot_input)
+        Text2Speech(str(bot_input))
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        print("Error Happened")
+
+def Text2Speech(sentence):
+    tts = gTTS(sentence ,lang="ar")
+    tts.save("Text2Speech.mp3")
+    playsound.playsound("Text2Speech.mp3")
+    os.remove("Text2Speech.mp3")
+
+def main():
+    User_input = input("Enter train or Prediction: ").lower()
+    if User_input == 't':
+        train()
+    elif User_input == 'p':
+        predict()
+    else:
+        print('Wrong Input!')
 if __name__ == '__main__':
     main()
 
